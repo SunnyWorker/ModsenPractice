21cf97b4cbb30db0fc4916783ae8c9a19683eebc
SunnyWorker <artem.khotin@gmail.com>
2023-06-09 02:36:37 +0300

    Unit tests for converter created

---
+++ src/test/java/org/example/ExchangeCalculatorTest.java
@@ -0,0 +1,56 @@
+package org.example;
+
+import org.example.converter.interfaces.IConverter;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExchangeCalculatorTest {
+
+    IConverter converter;
+
+    @Test
+    public void toRublesSimpleOperationTest() {
+        assertEquals("60,00р", converter.performOperation("toRubles($1,00  )"));
+        assertEquals("90,00р", converter.performOperation(" toRubles($1,50) "));
+    }
+
+    @Test
+    public void toDollarsSimpleOperationTest() {
+        assertEquals("$1,00", converter.performOperation("toDollars(  60,00р)"));
+        assertEquals("$1,50", converter.performOperation(" toDollars(90,00р  )"));
+    }
+
+    @Test
+    public void rublesSummarySimpleOperationTest() {
+        assertEquals("150,00р", converter.performOperation("60,00р + 90,00р"));
+        assertEquals("4,22р", converter.performOperation("  1,25р +   2,97р"));
+        assertEquals("4,22р", converter.performOperation(" ( ( (1,25р +   2,97р) ) ) "));
+    }
+
+    @Test
+    public void dollarsSummarySimpleOperationTest() {
+        assertEquals("$2,00", converter.performOperation("$1,00 + $1,00"));
+        assertEquals("$7,85", converter.performOperation("  $1,25 +   $6,60"));
+        assertEquals("$7,85", converter.performOperation(" ( ( ($1,25 +   $6,60) ) ) "));
+    }
+
+    @Test
+    public void rublesExpressionOperationsTest() {
+        assertEquals("0,01р", converter.performOperation("60,00р + 90,00р - 149,99р"));
+        assertEquals("150,00р", converter.performOperation("60,00р  +  toRubles($1,50)"));
+    }
+
+    @Test
+    public void dollarsExpressionOperationsTest() {
+        assertEquals("$0,50", converter.performOperation("$1,50 + $1,50 - $2,50"));
+        assertEquals("$-0,50", converter.performOperation("$1,50  -  toDollars(120,00р)"));
+    }
+
+    @Test
+    public void innerOperationsTest() {
+        assertEquals("$4,00", converter.performOperation("toDollars(120,00р + 120,00р)"));
+        assertEquals("$1,50", converter.performOperation("toDollars(toRubles($1,50))"));
+        assertEquals("$4,00", converter.performOperation("toDollars(toRubles($1,50  + $2,50))"));
+    }
+}
